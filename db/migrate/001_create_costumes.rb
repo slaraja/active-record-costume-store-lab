21e52bc79d14868955fae48820b942e94596bc27
Stephanie La Raja <stephanielaraja@gmail.com>
2021-03-23 08:32:55 -0400

    commit tree b306ebcebc6e69bd99eb50a102349cf980faf7a9

--- db/migrate/001_create_costumes.rb
+++ db/migrate/001_create_costumes.rb
@@ -1,3 +1,15 @@
+class CreateCostumes < ActiveRecord::Migration[4.2]
+    def change
+        create_table :costumes do |t|
+          t.string :name
+          t.integer :price
+          t.string :size
+          t.string  :image_url
+          t.timestamps
+        end
+    end
+end
+
 # Create a class and inherit from ActiveRecord::Migration
 
 # By convention, the class name should match the part of the
